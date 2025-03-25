26b79cfb467681e6f7ca60a25558eeb96d916b90
kmannes0118 <kmannes@stumail.jccc.edu>
2025-03-25 14:09:10 -0500

    Create script.js

---
+++ Chapter3Project/script.js
@@ -0,0 +1,24 @@
+// Kendra Mannes
+// 03/25/2025
+
+var fullName = "Kendra Mae Mannes";
+console.log("Full Name:", fullName);
+
+var desiredSalary = 75000;
+console.log("Desired Salary:", desiredSalary);
+
+var isVeteran = false; 
+console.log("Veteran Status:", isVeteran);
+
+var friends = ["Luke", "Parker", "Maddie"];
+console.log("Friends:", friends);
+
+var friendsSalaries = [65000, 70000, 80000];
+console.log("Friends' Salaries:", friendsSalaries);
+
+var anotherFriend = {
+    firstName: "Becky",
+    lastName: "Davenport",
+    desiredSalary: 90000
+};
+console.log("Another Friend:", anotherFriend);
